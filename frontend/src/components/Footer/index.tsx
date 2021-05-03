ec7050d437f9bd08132d5d5999d4dc8a39ac3b74
Yago Ananias <yago_souza92@yahoo.com.br>
2021-05-03 11:15:42 -0300

    Componentes básicos estáticos

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">App desenvolvido por <a href="https://github.com/yagoananias" target="_blank" rel="noreferrer">Yago Ananias</a></p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
