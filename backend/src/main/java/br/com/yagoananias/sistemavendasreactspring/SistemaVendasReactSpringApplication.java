03d48ff467c6092820eb24c60db7b69711fe2e56
Yago Ananias <yago_souza92@yahoo.com.br>
2021-05-03 08:59:19 -0300

    Criando o projeto

---
+++ backend/src/main/java/br/com/yagoananias/sistemavendasreactspring/SistemaVendasReactSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.yagoananias.sistemavendasreactspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaVendasReactSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaVendasReactSpringApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/yagoananias/sistemavendasreactspring/SistemaVendasReactSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.yagoananias.sistemavendasreactspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaVendasReactSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
