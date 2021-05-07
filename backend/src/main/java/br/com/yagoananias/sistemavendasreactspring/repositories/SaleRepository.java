56a33647a5e0f885a94f22dd5039604f2f66bc7c
Yago Ananias <yago_souza92@yahoo.com.br>
2021-05-07 16:12:52 -0300

    Group by search

--- backend/src/main/java/br/com/yagoananias/sistemavendasreactspring/repositories/SaleRepository.java
+++ backend/src/main/java/br/com/yagoananias/sistemavendasreactspring/repositories/SaleRepository.java
@@ -1,9 +1,22 @@
 package br.com.yagoananias.sistemavendasreactspring.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import br.com.yagoananias.sistemavendasreactspring.dto.SaleSuccessDTO;
+import br.com.yagoananias.sistemavendasreactspring.dto.SaleSumDTO;
 import br.com.yagoananias.sistemavendasreactspring.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+	
+	@Query("SELECT new br.com.yagoananias.sistemavendasreactspring.dto.SaleSumDTO(obj.seller, SUM(obj.amount)) "
+			+ " FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSumDTO> amountGroupedBySeller();
+	
+	@Query("SELECT new br.com.yagoananias.sistemavendasreactspring.dto.SaleSuccessDTO(obj.seller, SUM(obj.visited), SUM(obj.deals)) "
+			+ " FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSuccessDTO> successGroupedBySeller();
 
 }
--- backend/src/main/java/br/com/yagoananias/sistemavendasreactspring/services/SaleService.java
+++ backend/src/main/java/br/com/yagoananias/sistemavendasreactspring/services/SaleService.java
@@ -1,5 +1,7 @@
 package br.com.yagoananias.sistemavendasreactspring.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.yagoananias.sistemavendasreactspring.dto.SaleDTO;
+import br.com.yagoananias.sistemavendasreactspring.dto.SaleSuccessDTO;
+import br.com.yagoananias.sistemavendasreactspring.dto.SaleSumDTO;
 import br.com.yagoananias.sistemavendasreactspring.entities.Sale;
 import br.com.yagoananias.sistemavendasreactspring.repositories.SaleRepository;
 import br.com.yagoananias.sistemavendasreactspring.repositories.SellerRepository;
@@ -26,6 +30,16 @@ public class SaleService {
 		sellerRepository.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller() {
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller() {
+		return repository.successGroupedBySeller();
 	}
 
 }
